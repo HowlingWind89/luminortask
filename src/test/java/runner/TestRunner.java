b4206ddc9d6ebcb43299645b74e08b274315a091
HowlingWind89 <nameless89@mail.ru>
2025-04-05 16:57:20 +0300

    configured TestNG TestRunner class now it is also can be used for test excution

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,12 +2,20 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
-        tags = "Luminor",
-        features = {"src/test/resources/features"},
-        glue = {"testSteps"},
-        plugin = {"pretty", "html:target/cucumber-report.html"}
+        tags = "@Luminor",
+        features = "src/test/resources/features",
+        glue = "testSteps",
+        plugin = {"pretty",
+                "html:target/cucumber-report.html",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+        @Test
+        public void dummy() {
+                // этот метод просто "помечает" класс как тест
+        }
 }
