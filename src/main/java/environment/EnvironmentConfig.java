315d820856b087194bd72ccfb9fc9023357d4795
HowlingWind89 <nameless89@mail.ru>
2025-04-06 00:53:48 +0300

    making test working in headless mode

--- src/main/java/environment/EnvironmentConfig.java
+++ src/main/java/environment/EnvironmentConfig.java
@@ -2,6 +2,8 @@ package environment;
 
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class EnvironmentConfig {
@@ -9,16 +11,26 @@ public class EnvironmentConfig {
     public void setUp() {
 
         WebDriverManager.chromedriver().setup();
-        System.setProperty("selenide.browser", "Chrome");
+        ChromeOptions options = new ChromeOptions();
 
+        boolean isCI = "true".equalsIgnoreCase(System.getenv("CI"));
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-blink-features=AutomationControlled");
+            options.addArguments("--window-size=1920,1080");
+
+            options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                    "(KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+        }
+
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = true;
         Configuration.timeout = 10000;
+        Configuration.browserCapabilities = options;
 
-        String ciEnv = System.getenv("CI");
-        //Configuration.headless = "true".equalsIgnoreCase(ciEnv);
-
-        System.out.println("[Browser mode] Headless = " + Configuration.headless);
+        System.out.println("[Browser mode] Headless = " + isCI);
 
         open(WebURL.IMDB);
     }
